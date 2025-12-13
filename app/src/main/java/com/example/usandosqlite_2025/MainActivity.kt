bcf4a033d08b6b2ab8ba84cfa10f39f777f9feda
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2025-12-13 15:01:00 -0300

    Código MainActivity já prevendo binding e criação do objeto que acessa o SQLite

--- app/src/main/java/com/example/usandosqlite_2025/MainActivity.kt
+++ app/src/main/java/com/example/usandosqlite_2025/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.usandosqlite_2025
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.usandosqlite_2025.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var banco: SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        banco = SQLiteDatabase.openOrCreateDatabase(
+            "bdfile.sqlite",
+            null
+        )
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
